1380953f2639ce05e9d56c101b001290d107b976
Sergey Sidorchick <whitekrokodile@gmail.com>
2013-11-03 23:07:56 +0300

    Todo individual view added.

---
+++ js/views/todo.js
@@ -0,0 +1,44 @@
+var app || {};
+
+app.TodoView = Backbone.View.extend({
+	tagName: "li",
+	template: _.template($("#item-template")),
+	events: {
+		"dbClick label": "edit",
+		"keypress .edit": "updateOnEnter",
+		"blur .edit": "close"
+	},
+
+	initialize: function() {
+		this.listenTo(this.model, "change", this.render);
+	},
+
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		this.$input = this.$(".edit");
+		return this;
+	},
+
+	edit: function() {
+		this.$el.addClass("editing");
+		this.$input.focus();
+	},
+
+	close: function() {
+		var value = this.$input.val().trim();
+
+		if (value) {
+			this.model.save({
+				title: value
+			});
+		}
+
+		this.$input.removeClass(".editing");
+	},
+
+	updateOnEnter: function(e) {
+		if (e.which === ENTER_KEY) {
+			this.close();
+		}
+	}
+});
